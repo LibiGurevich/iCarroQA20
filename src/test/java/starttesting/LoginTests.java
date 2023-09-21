f24dddcfaa2ae416ae2f1158b9fb7c4e5b239e06
Libi Gurevich <l.zholudeva@gmail.com>
2023-09-21 13:16:54 +0300

    modif

--- src/test/java/starttesting/LoginTests.java
+++ src/test/java/starttesting/LoginTests.java
@@ -1,8 +1,11 @@
 package starttesting;
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,7 +24,31 @@ public class LoginTests {
     }
 
     @Test
-    public void loginPositive(){}
+    public void loginPositive(){
+
+        //in //h2[@class='message'] - Logged in success
+
+        WebElement btnLogin = driver.findElement(By.xpath(""));
+        btnLogin.click();
+        WebElement inputEmail = driver.findElement(By.xpath(""));
+        inputEmail.click();
+        inputEmail.clear();
+        inputEmail.sendKeys();
+
+        WebElement inputPassword = driver.findElement(By.xpath(""));
+        inputPassword.click();
+        inputPassword.clear();
+        inputPassword.sendKeys();
+
+        WebElement btnYalla = driver.findElement(By.xpath(""));
+        btnYalla.click();
+
+        WebElement textMessagePopUph2 = driver.findElement(By.xpath("//h2[@class='message']"));
+        String textMessageH2 = textMessagePopUph2.getText().trim().toUpperCase();
+        String expectedResult = "Logged in success".toUpperCase();
+
+        Assert.assertEquals(textMessageH2, expectedResult);
+    }
 
 
 
