162d46d8f56c409d2229392902f3a855a76b6072
Libi Gurevich <l.zholudeva@gmail.com>
2023-09-21 12:22:32 +0300

    modif

--- src/test/java/starttesting/LoginTests.java
+++ src/test/java/starttesting/LoginTests.java
@@ -1,2 +1,34 @@
-package starttesting;public class LoginTests {
+package starttesting;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void preconditions(){
+        driver = new ChromeDriver();
+        driver.navigate().to("https://ilcarro.web.app/search");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void loginPositive(){}
+
+
+
+    @AfterClass
+    public void postConditions(){
+
+        driver.quit();
+    }
+
 }
